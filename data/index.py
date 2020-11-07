dfd4493b96c86ab36d7de62adc2e2f0ffd2190e9
Aleksandr Tereshchenko <aleksandr.tereshchenko@reaktor.com>
2020-11-07 12:00:10 +0200

    Fix broken data

--- data/index.py
+++ data/index.py
@@ -1,26 +1,30 @@
 import csv, json
 
-data = []
-with open('./dude3.csv') as csvfile:
-  spamreader = csv.reader(csvfile, delimiter=';')
-  for row in spamreader:
-    print(row)
-    data.append({
-      "category": row[0],
-      "timestamp": row[1],
-      "arvopvm": row[2],
-      "kirjauspvm": row[3],
-      "maksupvm": row[4],
-      "accountNumber": int(row[5]),
-      "amount": float(row[6]),
-      "saldo": float(row[7]),
-      "vientiselitekd": int(row[8]),
-      "taplajikd": int(row[9]),
-      "bicSaaja": row[10],
-      "viite": int(row[11]),
-      "ibanSaaja": int(row[12]),
-      "counterpartyAccountId": int(row[13]),
-    })
 
-with open("./dude3.json", 'w', encoding='utf-8') as f:
-  json.dump(data, f, ensure_ascii=False, indent=2)
+for i in range(1, 14):
+  data = []
+  with open('./transactions.csv') as csvfile:
+    spamreader = csv.reader(csvfile, delimiter=';')
+    for idx, row in enumerate(spamreader):
+      if idx == 0 or len(row) < 14:
+        continue
+      if int(row[5]) == i:
+        data.append({
+          "category": row[0],
+          "timestamp": row[1],
+          "arvopvm": row[2],
+          "kirjauspvm": row[3],
+          "maksupvm": row[4],
+          "accountNumber": int(row[5]),
+          "amount": float(row[6]),
+          "saldo": float(row[7]),
+          "vientiselitekd": int(row[8]),
+          "taplajikd": int(row[9]),
+          "bicSaaja": row[10],
+          "viite": int(row[11]),
+          "ibanSaaja": int(row[12]),
+          "counterpartyAccountId": int(row[13]),
+        })
+
+  with open("./dude%s.json" % i, 'w', encoding='utf-8') as f:
+    json.dump(data, f, ensure_ascii=False, indent=2)
